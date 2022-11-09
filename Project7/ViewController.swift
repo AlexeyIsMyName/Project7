cce04b22453486d10f78abddb127ef5fe2110bf6
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-11-09 23:10:25 +0300

    use JSONDecoder to parse JSOn file gotten from the Internet

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -17,24 +17,36 @@ class ViewController: UITableViewController {
         // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                // we're OK to parse!
+        DispatchQueue.global().async {
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    // we're OK to parse!
+                    DispatchQueue.main.async {
+                        self.parse(json: data)
+                    }
+                }
             }
         }
     }
-}
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
 
-// MARK: - Data Source
-extension ViewController {
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return petitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 }
