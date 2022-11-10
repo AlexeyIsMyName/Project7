bf9defc0894de21e4527aa34ac810d40b41fe16c
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-11-10 12:17:07 +0300

    make different URLs for different TabBars

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -14,19 +14,32 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//         let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
-        DispatchQueue.global().async {
+//         let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+//        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+//        DispatchQueue.global().async {
             if let url = URL(string: urlString) {
                 if let data = try? Data(contentsOf: url) {
                     // we're OK to parse!
-                    DispatchQueue.main.async {
+//                    DispatchQueue.main.async {
                         self.parse(json: data)
-                    }
+                        return
+//                    }
                 }
             }
-        }
+//        }
+        
+        showError()
     }
     
     func parse(json: Data) {
@@ -36,6 +49,15 @@ class ViewController: UITableViewController {
             petitions = jsonPetitions.results
             tableView.reloadData()
         }
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error",
+                                   message: "There was a problem loading the feed; please check your connection and try again.",
+                                   preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
